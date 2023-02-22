4b924ae4f377c5a5b0a6001de0b5a6446752cff8
understreck <pqfrml@gmail.com>
2023-02-22 15:13:24 +0100

    Finish the script according to instructions

--- python-scripts/create_directly.py
+++ python-scripts/create_directly.py
@@ -1,7 +1,11 @@
 import ROOT
 import json
 
+ROOT.gInterpreter.LoadMacro('./root-scripts/MyClass.C')
 ROOT.gInterpreter.LoadMacro('./root-scripts/createFromJsonString.C')
 
 def create_directly(className : str, args : list):
     return ROOT.createFromJsonString(json.dumps({"class" : className, "args" : args}))
+
+obj_1 = create_directly("MyClass", ["first", "The First Instance", 1])
+obj_1.Print()
